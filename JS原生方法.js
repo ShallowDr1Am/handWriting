426f309d2ca76d158f6efe7489bc7b7e6e15be0d
lightzhou_i <lightzhou_i@didiglobal.com>
2023-08-17 18:40:16 +0800

    test

--- JS原生方法.js
+++ JS原生方法.js
@@ -11,3 +11,34 @@ Array.prototype.flat = function (deep = 1) {
   }
   return res
 }
+
+// instanceof
+function myInstanceOf(left, right) {
+  let proto = Object.getPrototypeOf(left),
+    prototype = right.prototype;
+  while (true) {
+    if (!proto) return false
+    if (proto === prototype) return true
+    proto = Object.getPrototypeOf(proto)
+  }
+}
+
+Array.prototype._map = function (cb, thisBinding) {
+  // 排除回调非函数情况
+  if (typeof cb !== "function") {
+    throw new TypeError(`${cb} is not a function`);
+  }
+  // 排除this为非可迭代对象情况
+  if (this == null || typeof this[Symbol.iterator] !== "function") {
+    throw new TypeError(`${this} is not a iterable`);
+  }
+  // 将可迭代对象转换成数组
+  const array = [...this];
+  const result = [];
+  // 执行遍历并回调
+  for (let i = 0; i < array.length; i++) {
+    result.push(cb.call(thisBinding, array[i], i, this));
+  }
+  return result;
+};
+console.log(arr._map(item => item + 1, [3, 4]))
